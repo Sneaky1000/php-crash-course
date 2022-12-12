bd6820ca9d6170732ee1ba8493f064e8b1f41fb4
Chase Wheeler <cwheeler0204@gmail.com>
2022-12-12 10:29:32 -0800

    Fix 15_file_uploading file

--- 15_file_uploading.php
+++ 15_file_uploading.php
@@ -1,44 +1,59 @@
 <?php
+  /* ----------- File upload ---------- */
 
-/* ----------- Exceptions ----------- */
 
-/*
-  PHP has an exception model similar to that of other programming languages. An exception can be thrown, and caught ("catched") within PHP. Code may be surrounded in a try block, to facilitate the catching of potential exceptions. Each try must have at least one corresponding catch or finally block.
-*/
+  $allowed_ext = array('png', 'jpg', 'jpeg', 'gif');
 
-function inverse($x) {
-  if (!$x) {
-    throw new Exception('Division by zero.');
-  }
-  return 1/$x;
-}
+ if(isset($_POST['submit'])) {
+   // Check if file was uploaded
+   if(!empty($_FILES['upload']['name'])) {
+    $file_name = $_FILES['upload']['name'];
+    $file_size = $_FILES['upload']['size'];
+    $file_tmp = $_FILES['upload']['tmp_name'];
+    $target_dir = "uploads/${file_name}";
+    // Get file extension
+    $file_ext = explode('.', $file_name);
+    $file_ext = strtolower(end($file_ext));
+    // echo $file_ext;
 
-// echo inverse(0); // Causes an exception to be thrown and stops script execution
+    // Validate file type/extension
+    if(in_array($file_ext, $allowed_ext)) {
+      // Validate file size
+      if($file_size <= 1000000) { // 1000000 bytes = 1MB
+        // Upload file
+        move_uploaded_file($file_tmp, $target_dir);
 
-// Handles the exception
-// try {
-//   echo inverse(5) . ' ';
-//   echo inverse(0) . ' '";
-// } catch (Exception $e) {
-//   echo 'Caught exception: ',  $e->getMessage(), ' ';
-// }
+        // Success message
+        echo '<p style="color: green;">File uploaded!</p>';
+      } else {
+        echo '<p style="color: red;">File too large!</p>';
+      }
+    } else {
+      $message = '<p style="color: red;">Invalid file type!</p>';
+    }
+   } else {
+     $message = '<p style="color: red;">Please choose a file</p>';
+   }
+ }
+  ?>
 
-// Finally block is executed regardless of whether an exception is thrown or not
-
-try {
-  echo inverse(5) . ' ';
-} catch (Exception $e) {
-  echo 'Caught exception: ',  $e->getMessage(), ' ';
-} finally {
-  echo 'First finally ';
-}
-
-try {
-  echo inverse(0) . ' ';
-} catch (Exception $e) {
-  echo 'Caught exception: ',  $e->getMessage(), ' ';
-} finally {
-  echo "Second finally ";
-}
-
-echo 'Hello World';
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File Upload</title>
+  </head>
+  <body>
+    <?php echo $message ?? null; ?>
+  <form action="<?php echo htmlspecialchars(
+    $_SERVER['PHP_SELF']
+  ); ?>" method="post" enctype="multipart/form-data">
+    Select image to upload:
+  <input type="file" name="upload">
+  <input type="submit" value="Submit" name="submit">
+</form>
+  </body>
+  </html>
+  
